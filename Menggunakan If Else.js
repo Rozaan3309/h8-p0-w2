d52c14b9cf102b128f3afed1b995961065786cc8
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-02-26 00:17:52 +0700

    added If Else and Switch-Case Challenges

---
+++ Menggunakan If Else.js
@@ -0,0 +1,21 @@
+var nama = 'Budi'
+var peran = 'Penyihir'
+
+if (!nama && !peran) {
+    console.log('Nama harus diisi!');
+} else if(nama && !peran) {
+    console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+} else if(nama && peran === 'Ksatria') {
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+} else if(nama && peran === 'Tabib') {
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka.');
+} else if(nama && peran === 'Penyihir') {
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+} else if(!nama && peran) {
+    console.log('Wahai ' + peran + ', mohon sebutkan nama mu.');
+} else {
+    console.log('Halo ' + nama + ', peran yang kamu tulis tidak tersedia.');
+}
---
+++ Menggunakan Switch-Case.js
@@ -0,0 +1,58 @@
+var hari = 17
+var bulan = 8
+var tahun = 1945
+
+switch(bulan) {
+    case 1:
+        bulan = 'Januari'
+        break;
+    case 2:
+        bulan = 'Februari'
+        break;
+    case 3:
+        bulan = 'Maret'
+        break;
+    case 4:
+        bulan = 'April'
+        break;
+    case 5:
+        bulan = 'Mei'
+        break;
+    case 6:
+        bulan = 'Juni'
+        break;
+    case 7:
+        bulan = 'Juli'
+        break;
+    case 8:
+        bulan = 'Agustus'
+        break;
+    case 9:
+        bulan = 'September'
+        break;
+    case 10:
+        bulan = 'Oktober'
+        break;
+    case 11:
+        bulan = 'November'
+        break;
+    case 12:
+        bulan = 'Desember'
+        break;
+}
+
+var tanggal = hari + ' ' + bulan + ' ' + tahun
+
+
+if (hari > 31 || hari < 1){
+    tanggal = 'masukan hari yang benar!'
+    console.log(tanggal);
+} else if (bulan > 12 || bulan < 1){
+    tanggal = 'masukan bulan yang benar!'
+    console.log(tanggal);
+} else if (tahun > 2200 || tahun < 1900){
+    tanggal = 'masukan tahun antara 1900 sampai 2200!'
+    console.log(tanggal);
+} else {
+    console.log(tanggal);
+} 
